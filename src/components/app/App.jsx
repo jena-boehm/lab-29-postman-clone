8983ba1132a2638409cc064c91ce5a85c44c30f0
Jena Boehm <boehmjena@gmail.com>
2021-02-05 15:18:41 -0800

    basic file structure

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import Restful from '../../containers/Restful';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <Restful />;
 }
---
+++ src/components/form/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Form = () => {
+    
+};
+
+Form.propTypes = {
+    
+};
+
+export default Form;
