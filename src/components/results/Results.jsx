3f76ab309bb13e82c191c1449676563894e28d70
Jena Boehm <boehmjena@gmail.com>
2021-02-05 16:37:10 -0800

    build out presentational components

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactJSON from 'react-json-view';
 
-const Results = () => {
-    
-};
+const Results = ({ results }) => (
+  <ReactJSON src={results} />
+);
 
 Results.propTypes = {
-    
+  results: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired
 };
 
 export default Results;
