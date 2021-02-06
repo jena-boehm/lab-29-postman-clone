16e08de48bd1580ff0186fd8456c6e0ebdf811d4
Jena Boehm <boehmjena@gmail.com>
2021-02-05 17:54:04 -0800

    things rendering on page yay

--- src/containers/Restful.jsx
+++ src/containers/Restful.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import Form from '../components/form/Form';
+import Header from '../components/header/Header';
+import Results from '../components/results/Results';
 
 export default class Restful extends Component {
+  state = {
+    url: '',
+    method: '',
+    body: '',
+    results: { 'Welcome!': 'Please make a fetch' }
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
+
+
   render() {
+    const { url, method, body, results } = this.state;
+
     return (
-      <div>
-            Hello    
-      </div>
+      <>
+        <Header /> 
+        <Form 
+          url={url}
+          method={method}
+          body={body}
+          onChange={this.handleChange}
+          onSubmit={this.handleSubmit} />
+        <Results results={results} />
+      </>
     );
   }
 }
